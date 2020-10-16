0b808a4b05c6b6b5c236cc3cd10ec9c072d88614
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-16 12:47:17 +0530

    My first Commit

---
+++ Namaskara.java
@@ -0,0 +1,24 @@
+class Wish{
+	public static void main(String[] param)
+	{
+		System.out.println("Namaskara");
+}}
+class Camera
+{
+	public static void main(String[] param)
+	{
+		System.out.println("Camera is Kodak");
+		System.out.println("price is lot");
+	}
+		
+	
+
+}
+class Weather{
+	public static void main(String[] param)
+	{
+		System.out.println("Dharwad is a cold city");
+	}
+}
+		
+	
