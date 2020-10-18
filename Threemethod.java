f8d68b9a992f41d0db89bc5e98203540af5ab008
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-18 20:49:55 +0530

    Add files via upload
    second commit

---
+++ Threemethod.java
@@ -0,0 +1,17 @@
+class Main
+{
+public static void main(String[] params)
+{
+System.out.println("Method is Successfull");
+myMusic();
+}
+public static void myMusic()
+{
+System.out.println("Would you start the music for me?");
+drinking();
+}
+public static void drinking()
+{
+System.out.println("Drinking more water is good for health");
+} 	
+}
