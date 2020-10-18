f7399ff0d9f543e48554e25d09880c8d02fe0c11
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-18 21:08:46 +0530

    4th commit

---
+++ Twomethods.java
@@ -0,0 +1,16 @@
+class Read
+{
+public static void main(String[] params)
+{
+System.out.println("World");
+write();
+write();
+write();
+}
+public static void write()
+{
+	System.out.println("Requirements");
+}
+}
+
+
