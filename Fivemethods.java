d815092bdec77ddba6f45c28dea0629ab1d8e8fc
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-18 20:58:12 +0530

    Third commit

---
+++ Fivemethods.java
@@ -0,0 +1,31 @@
+class Write
+{
+public static void main(String[] params)
+{
+System.out.println("Learning");
+teaching();
+}
+public static void teaching()
+{
+System.out.println("Failure is a first step of success");
+motivation();
+}
+public static void motivation()
+{
+System.out.println("A talented athlete who lacks Motivation ");
+friends();
+} 
+public static void friends()
+{
+System.out.println("A friend in a need's friend indeed");
+playing();
+}
+public static void playing()
+{
+System.out.println("Outdoor games");
+}
+}
+
+
+
+	
