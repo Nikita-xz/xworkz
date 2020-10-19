e09ac2b7ab8fb02ee390766020832b540afe09d5
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-19 21:47:41 +0530

    Add files via upload

---
+++ Friend.java
@@ -0,0 +1,35 @@
+public class Friend {
+public static void main(String[] params){
+System.out.println("Measurements");
+byte fingers;
+fingers=15;
+short daysInAYear;
+daysInAYear=365;
+int numberOfEngineersInIndia;
+numberOfEngineersInIndia=6214;
+long populationOfIndia;
+populationOfIndia=423498745;
+float percentageOfStudents;
+percentageOfStudents=8.57f;
+double chargeOfElectron;
+chargeOfElectron=32.12345d;
+boolean earthIsFlat;
+earthIsFlat=false;
+char grade;
+grade='A';
+System.out.println("fingers");
+System.out.println("daysInAYear");
+System.out.println("numberOfEnineersInIndia");
+System.out.println("populationOfIndia");
+System.out.println("percentageOfStudents");
+System.out.println("chargeOfElectron");
+System.out.println("earthIdFlat");
+System.out.println("grade");
+}
+}
+
+
+
+
+
+
