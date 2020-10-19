d6e0cef4a8784cddbc211b5bfdcf3d9b10853072
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-19 21:50:54 +0530

    My 5th commit

---
+++ Fruit.java
@@ -0,0 +1,30 @@
+public class Fruit {
+public static void main(String[] args){
+System.out.println("Requirments");	
+byte myNum;
+myNum=100;
+short numberOfCarsInFactory;
+numberOfCarsInFactory=2200;
+int height;
+height=500000;
+long numberOfPeopleInCity;
+numberOfPeopleInCity=120000;
+float fdInterest;
+fdInterest=7.2f;
+double squareRootOf14;
+squareRootOf14=3.14267d;
+boolean isJavaFun;
+isJavaFun=true;
+char vowels;
+vowels='I';
+System.out.println("myNum");
+System.out.println("numberOfCarsInFactory");
+System.out.println("heigt");
+System.out.println("numberOfPeopleInCity");
+System.out.println("fdInterest");
+System.out.println("");
+System.out.println("isJavaFun");
+System.out.println("vowels");
+System.out.println("greeting");
+}
+}
