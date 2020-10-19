d0ef2860dae910742cf068832477003fc41f9f72
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-19 21:51:31 +0530

    6th commit

---
+++ Joy.java
@@ -0,0 +1,38 @@
+public class Joy
+{
+public static void main(String[] param)
+{
+System.out.println("Requirments");
+byte oneDozen;
+oneDozen=12;
+short daysInAMonth;
+daysInAMonth=31;
+int numbeOfSpeciesInIndia;
+numbeOfSpeciesInIndia=136000;
+long populationOfMalesInIndia;
+populationOfMalesInIndia=54197555;
+float unEmploymentRate;
+unEmploymentRate=18.8f;
+double areaOfEarth;
+areaOfEarth=345.567d;
+boolean skyIsBlue;
+skyIsBlue=true;
+char position;
+position='A';
+System.out.println("oneDozen");
+System.out.println("daysInAMonth");
+System.out.println("numberOfSpeciesInIndia");
+System.out.println("populationOfMalesInIndia");
+System.out.println("unEmploymentRate");
+System.out.println("areaOfEarth");
+System.out.println("skyIsBlue");
+System.out.println("position");
+}
+}
+
+
+
+
+
+
+
