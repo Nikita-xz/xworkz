d34dc83b20f98b336b03b428a413d815ca0aa7a6
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-19 21:52:05 +0530

    7th commit

---
+++ World.java
@@ -0,0 +1,29 @@
+public class World {
+public static void main(String[] params){
+System.out.println("Difination");
+byte pairOfSocks;
+pairOfSocks=120;
+short numberOfStudentsInClass;
+numberOfStudentsInClass=80;
+int length;
+length=20;
+long numberOfStudentsInUniversity;
+numberOfStudentsInUniversity=63000;
+float interestRate;
+interestRate=8.5f;
+double pi;
+pi=3.14269d;
+boolean earthRevolvesAroundSun;
+earthRevolvesAroundSun=true;
+char letter;
+letter='N';
+System.out.println("pairOfSocks");
+System.out.println("numberOfStudentsInClass");
+System.out.println("length");
+System.out.println("numberOfStudentsInUniversity");
+System.out.println("interestRate");
+System.out.println(" pi");
+System.out.println("earthRevolvesAroundSun");
+}
+}
+
