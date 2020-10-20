da93f0ad168c790dfa015efc172ad670fe61e47c
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-20 20:15:24 +0530

    Add files via upload

---
+++ Picture.java
@@ -0,0 +1,14 @@
+public class Picture{
+public static void main(String[] args)
+{
+email("nikita@gmail.com");
+programmingLanguage("java");
+}
+public static void email(String email){
+System.out.println("email:" +email);
+}
+public static void programmingLanguage(String programmingLanguage ){
+System.out.println("programmingLanguage:" +programmingLanguage);
+}
+}
+
