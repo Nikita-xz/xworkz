f9cafd56356148a8e7f2f2e6ef43a0d5c2add66c
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-20 20:13:50 +0530

    Add files via upload

---
+++ People.java
@@ -0,0 +1,12 @@
+public class People{
+public static void main(String[] args){
+final String adhardCardNum="8654765478";
+final String mobileNumber="8050172934";
+final String email="xworkz@gmail.com";
+System.out.println("adhardCardNum: "+adhardCardNum);
+System.out.println("mobileNumber: "+mobileNumber);
+System.out.println("email: "+email);
+}
+}
+
+
