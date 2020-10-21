52210e435c58dc821a5cb40cc82e9b63821e0be2
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-21 21:02:46 +0530

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,22 @@
+public class Animals{
+public static void main(String[] args)
+{
+	System.out.println("Result");
+	arrayOfAnimals();
+}
+public static void arrayOfAnimals()
+{
+System.out.println("invoking method");
+boolean dogIsDomesticAnimal =true;
+boolean lionIsWildAnimal =false;
+boolean tigerIsWildAnimal =true;
+boolean cowIsDomesticAnimal =true;
+
+
+boolean[] animals ={dogIsDomesticAnimal,lionIsWildAnimal,tigerIsWildAnimal,cowIsDomesticAnimal};
+int sizeOfArray=animals.length;
+System.out.println("Total elements=" +sizeOfArray);
+boolean elementAtIndex2=animals[2];
+System.out.println("elementAtIndex two=" +elementAtIndex2);
+}
+}
