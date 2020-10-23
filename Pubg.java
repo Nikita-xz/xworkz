4e870744c94374a4f93bf0ab0c6ed24fd3642b40
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-10-23 21:33:45 +0530

    Add files via upload
     Toy.java

---
+++ Pubg.java
@@ -0,0 +1,12 @@
+public class Pubg{
+	
+	public static void supply()
+	{
+		System.out.println("supply is a method in Pubg class");
+	}
+	
+	public static void enemiesAhead()
+	{
+		System.out.println("enemiesAhead is a method in Pubg class");
+	}
+}
