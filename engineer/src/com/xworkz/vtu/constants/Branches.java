0d2d7c80a975dba485263496c36e1840cca6136e
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-12-25 00:26:17 +0530

    override
    override

---
+++ engineer/src/com/xworkz/vtu/constants/Branches.java
@@ -0,0 +1,8 @@
+package com.xworkz.vtu.constants;
+
+public enum Branches {
+	CS,IS,MECH,CIVIL,EC,EEE,TEXTILE,IT,AERO,CHEMICAL,ROBOTICS
+	
+}
+
+
---
+++ engineer/src/com/xworkz/vtu/engineer/EEEngineer.java
@@ -0,0 +1,24 @@
+package com.xworkz.vtu.engineer;
+
+import com.xworkz.vtu.constants.Branches;
+
+public class EEEngineer extends ECEngineer{
+	
+	private Branches branch=Branches.EEE;
+	
+	public void problemSolving() {
+		
+		super.problemSolving();
+		System.out.println("problem solved by EE");
+		//System.out.println("branch is"+this.branch);
+	}
+	public Branches getBranch() {
+		return branch;
+	}
+	public Branches getParentBranch() {
+		
+		return super.branch;
+	}
+	
+
+}
